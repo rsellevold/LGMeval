81215919c0cddcf519b74f204130e95e61cac03b
Raymond Sellevold <raymond.sellevold@gmail.com>
2020-09-11 09:25:07 +0200

    Created shared conda environment yaml file

--- main_proc.py
+++ main_proc.py
@@ -15,7 +15,7 @@ def hist_atm(info,atmdict):
 		"FSNSC", "FSNT", "ICEFRAC", "LHFLX", "PRECC", "PRECL", "PRECSC", "PRECSL", "PSL", "SHFLX", "TGCLDCWP",
 		"TGCLDIWP", "TMQ", "TS", "U250", "U500", "U850", "V250", "V500", "V850"]
 	src.merge_hist(info, "atm", varlist, atmdict, "h0", "monavg")
-	varlist = src.calcadd(info, "atm", varlist, atmdict, "monavg")
+	varlist = src.calcadd_atm(info, varlist, atmdict, "monavg")
 	src.mon2seas(info, "atm", varlist)
 	src.mon2ann(info, "atm", varlist)
 
@@ -29,6 +29,14 @@ def hist_atm(info,atmdict):
 	src.merge_hist(info, "atm", varlist, atmdict, "h1", "monavg", dimensions=3)
 	src.mon2seas(info, "atm", varlist)
 	src.mon2ann(info, "atm", varlist)
+
+def proc_atm():
+	# Make timeseries of all variables
+	varlist2d = ["ALBEDO", "CLDHGH", "CLDLOW", "CLDMED", "CLDTOT", "FLDS", "FLNS", "FLNSC", "FLNT", "FSDS", "FSDSC", "FSNS", 
+		"FSNSC", "FSNSC", "FSNT", "ICEFRAC", "LANDFRAC", "LHFLX", "LWCF", "OCNFRAC", "PRECC", "PRECL", "PRECSC", "PRECSL",
+		"PRECT", "PSL", "QREFHT", "RAIN", "SHFLX", "SNOW", "SWCF", "TGCLDCWP", "TGCLDIWP", "TMQ", "TREFHT", "TS", "U10",
+		"U250", "U500", "U850", "V250", "V500", "V850", "Z3"]
+	src.pp_atm.timeseries("atm",varlist)
 
 
 ##########
