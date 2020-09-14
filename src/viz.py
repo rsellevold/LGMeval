0f6e991a2bff65bb8856e16355eb1258539066c6
rsellevold <raymond.sellevold@gmail.com>
2020-09-14 12:57:11 +0200

    Update viz.py
    Addint titles to plots

--- src/viz.py
+++ src/viz.py
@@ -6,15 +6,16 @@ class viz:
         self.maintitle = None
         self.lefttitle = None
         self.righttitle = None
-
-    def map_default(self, ax, data, res):
+        
+    
+    def titles(self, ax):
         if self.maintitle is not None:
             if lefttitle is not None or righttitle is not None:
                 ax.set_title(self.maintitle, fontsize=self.fontsize, y=1.12)
             else:
                 ax.set_title(self.maintitle, fontsize=self.fontsize, y=1.04)
-
+                
         if self.lefttile is not None:
             ax.set_title(self.lefttitle, fontsize=self.fontsize-2, loc="left")
         if self.righttitle is not None:
-            ax.set_title(self.righttitle, fontsize=self.fontsize-2, loc="right") 
+            ax.set_title(self.righttitle, fontsize=self.fontsize-2, loc="right")
